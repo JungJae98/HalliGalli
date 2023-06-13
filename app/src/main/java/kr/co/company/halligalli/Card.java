c4497f4ad9ebfcb0a7a20a629216a2b613c98ecc
aabds <wogus8880@gmail.com>
2023-06-13 23:06:15 +0900

    과일 갯수 합이 5일 경우까지는 if문 작성완료 쓰레드를 이용해서 getCard1과 getCard2을 계속해서 호출해야함

--- app/src/main/java/kr/co/company/halligalli/Card.java
+++ app/src/main/java/kr/co/company/halligalli/Card.java
@@ -3,85 +3,101 @@ package kr.co.company.halligalli;
 import java.util.Random;
 
 public class Card {
+    private String card1 = "start"; //1번 플레이어의 카드
+    private String card2 = "start"; //2번 플레이어의 카드
 
+//    private String cardType(){
+//        Random random = new Random();
+//        int cardTypeRand = random.nextInt(4)+1; //카드 타입을 정하기 위한 랜덤변수
+//        String cardType = null; //카드의 이름이 저장될 변수
+//
+//        switch (cardTypeRand){
+//            case 1:
+//                cardType = "banana";
+//                break;
+//            case 2:
+//                cardType = "berry";
+//                break;
+//            case 3:
+//                cardType = "lime";
+//                break;
+//            case 4:
+//                cardType = "plot";
+//                break;
+//        }
+//        return cardType;
+//    }
+//
+//    private int cardNumber(){
+//        Random random = new Random();
+//        int cardNumberRand = random.nextInt(100)+1; //카드 숫자를 정하기 위한 랜덤변수
+//        int cardNumber; //카드의 숫자가 저장될 변수
+//
+//        if(cardNumberRand <= 35){
+//            return 1;
+//        } else if (cardNumberRand <= 56) {
+//            return 2;
+//        } else if (cardNumberRand <= 77) {
+//            return 3;
+//        } else if (cardNumberRand <= 91) {
+//            return 4;
+//        } else {
+//            return 5;
+//        }
+//    }
+//
+//    public void filpCard(){
+//        Random random = new Random();
+//        int cardTypeRand = random.nextInt(4)+1; //카드 타입을 정하기 위한 랜덤변수
+//        String cardType; //카드의 이름이 저장될 변수
+//
+//
+//        switch (cardTypeRand){
+//            case 1:
+//                cardType = "banana";
+//                break;
+//            case 2:
+//                cardType = "berry";
+//                break;
+//            case 3:
+//                cardType = "lime";
+//                break;
+//            case 4:
+//                cardType = "plot";
+//                break;
+//        }
+//
+//        int cardNumberRand = random.nextInt(100)+1; //카드 숫자를 정하기 위한 랜덤변수
+//        int cardNumber; //카드의 숫자가 저장될 변수
+//
+//        if(cardNumberRand <= 35){
+//
+//        } else if (cardNumberRand <= 56) {
+//
+//        } else if (cardNumberRand <= 77) {
+//
+//        } else if (cardNumberRand <= 91) {
+//
+//        } else if (cardNumberRand <= 100) {
+//
+//        }
+//
+//    }
 
-
-    private String cardType(){
-        Random random = new Random();
-        int cardTypeRand = random.nextInt(4)+1; //카드 타입을 정하기 위한 랜덤변수
-        String cardType = null; //카드의 이름이 저장될 변수
-        
-        switch (cardTypeRand){
-            case 1:
-                cardType = "banana";
-                break;
-            case 2:
-                cardType = "berry";
-                break;
-            case 3:
-                cardType = "lime";
-                break;
-            case 4:
-                cardType = "plot";
-                break;
-        }
-        return cardType;
+    public void setCard1(String v){ //플레이어1 카드 세팅
+        card1 = v;
     }
 
-    private int cardNumber(){
-        Random random = new Random();
-        int cardNumberRand = random.nextInt(100)+1; //카드 숫자를 정하기 위한 랜덤변수
-        int cardNumber; //카드의 숫자가 저장될 변수
-
-        if(cardNumberRand <= 35){
-            return 1;
-        } else if (cardNumberRand <= 56) {
-            return 2;
-        } else if (cardNumberRand <= 77) {
-            return 3;
-        } else if (cardNumberRand <= 91) {
-            return 4;
-        } else {
-            return 5;
-        }
+    public void setCard2(String v){ //플레이어2 카드 세팅
+        card2 = v;
     }
 
-    public void filpCard(){
-        Random random = new Random();
-        int cardTypeRand = random.nextInt(4)+1; //카드 타입을 정하기 위한 랜덤변수
-        String cardType; //카드의 이름이 저장될 변수
-        
+    public String getCard1(){ //플레이어1 카드 정보 가져옴
+        return card1;
+    }
 
-        switch (cardTypeRand){
-            case 1:
-                cardType = "banana";
-                break;
-            case 2:
-                cardType = "berry";
-                break;
-            case 3:
-                cardType = "lime";
-                break;
-            case 4:
-                cardType = "plot";
-                break;
-        }
-
-        int cardNumberRand = random.nextInt(100)+1; //카드 숫자를 정하기 위한 랜덤변수
-        int cardNumber; //카드의 숫자가 저장될 변수
-        
-        if(cardNumberRand <= 35){
-            
-        } else if (cardNumberRand <= 56) {
-            
-        } else if (cardNumberRand <= 77) {
-            
-        } else if (cardNumberRand <= 91) {
-            
-        } else if (cardNumberRand <= 100) {
-            
-        }
-
+    public String getCard2(){ //플레이어2 카드 정보 가져옴
+        return card2;
     }
 
 
--- app/src/main/java/kr/co/company/halligalli/MainActivity.java
+++ app/src/main/java/kr/co/company/halligalli/MainActivity.java
@@ -12,12 +12,15 @@ public class MainActivity extends AppCompatActivity {
     int setCardNum = 30;
     Game game = new Game();
     Player player1 = new Player(setCardNum); //기본 카드 숫자를 세팅
+    Player player2 = new Player(setCardNum);
+    Card card = new Card(); //카드 세팅과 저장할 곳
     ImageView p1_cardView; // 어떤 카드인지 띄워질 곳
     ImageView p2_cardView; //어떤 카드인지 띄워질 곳
     TextView p1_Tcard; //player1 카드 남은 갯수
-    TextView p2_Tcard; //player2 카
-    // 드 남은 갯수
-    String card[2]; //어떤 카드들이 나왔었는지 저장될 배열 0은 player1 1은 player2
+    TextView p2_Tcard; //player2 카드 남은 갯수
+//    String card[] = {"start", "start"}; //어떤 카드들이 나왔었는지 저장될 배열 0은 player1 1은 player2
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,14 +42,15 @@ public class MainActivity extends AppCompatActivity {
                 int imageResource = getResources().getIdentifier(cardFilp, "drawable", getPackageName());
                 if(imageResource != 0){
                     p1_cardView.setImageResource(imageResource); //0이 아닐때 이미지를 변경한다.
-                    card[0] = cardFilp; //
+                    card.setCard1(cardFilp);
+//                    card[0] = cardFilp; //
                 }
                 player1.desCardcount(); //카드수 1 감소
-                //텍스트 새롭게 설정
+
 
                 //카드 카운트가 0이 될 경우를 처리해야함
                 if(player1.getCardcount() != 0){
-                    // 텍스트를 새롭게 설정한다.
+                    // 남은카드 텍스트 업데이트
                     int newCard = player1.getCardcount();
                     p1_Tcard.setText(Integer.toString(newCard));
                 }else{  // 카드 카운트가 0이 될 경우
@@ -59,6 +63,42 @@ public class MainActivity extends AppCompatActivity {
                     //
                     //
                 }
+
+                // 과일 갯수의 합이 5일 경우
+                String card1 = card.getCard1();
+                String card2 = card.getCard2();
+                if ((card1.equals("banana1") && card2.equals("banana4"))
+                        || (card1.equals("banana2") && card2.equals("banana3"))
+                        || (card1.equals("banana3") && card2.equals("banana2"))
+                        || (card1.equals("banana4") && card2.equals("banana1"))
+                        || (card1.equals("plum1") && card2.equals("plum4"))
+                        || (card1.equals("plum2") && card2.equals("plum3"))
+                        || (card1.equals("plum3") && card2.equals("plum2"))
+                        || (card1.equals("plum4") && card2.equals("plum1"))
+                        || (card1.equals("lime1") && card2.equals("lime4"))
+                        || (card1.equals("lime2") && card2.equals("lime3"))
+                        || (card1.equals("lime3") && card2.equals("lime2"))
+                        || (card1.equals("lime4") && card2.equals("lime1"))
+                        || (card1.equals("strawberry1") && card2.equals("strawberry4"))
+                        || (card1.equals("strawberry2") && card2.equals("strawberry3"))
+                        || (card1.equals("strawberry3") && card2.equals("strawberry2"))
+                        || (card1.equals("strawberry4") && card2.equals("strawberry1"))
+                        || (card1.equals("banana5") && !(card2.equals("banana1") || card2.equals("banana2") || card2.equals("banana3") || card2.equals("banana4") || card2.equals("banana5")))
+                        || (card2.equals("banana5") && !(card1.equals("banana1") || card1.equals("banana2") || card1.equals("banana3") || card1.equals("banana4") || card1.equals("banana5")))
+                        || (card1.equals("plum5") && !(card2.equals("plum1") || card2.equals("plum2") || card2.equals("plum3") || card2.equals("plum4") || card2.equals("plum5")))
+                        || (card2.equals("plum5") && !(card1.equals("plum1") || card1.equals("plum2") || card1.equals("plum3") || card1.equals("plum4") || card1.equals("plum5")))
+                        || (card1.equals("lime5") && !(card2.equals("lime1") || card2.equals("lime2") || card2.equals("lime3") || card2.equals("lime4") || card2.equals("lime5")))
+                        || (card2.equals("lime5") && !(card1.equals("lime1") || card1.equals("lime2") || card1.equals("lime3") || card1.equals("lime4") || card1.equals("lime5")))
+                        || (card1.equals("strawberry5") && !(card2.equals("strawberry1") || card2.equals("strawberry2") || card2.equals("strawberry3") || card2.equals("strawberry4") || card2.equals("strawberry5")))
+                        || (card2.equals("strawberry5") && !(card1.equals("strawberry1") || card1.equals("strawberry2") || card1.equals("strawberry3") || card1.equals("strawberry4") || card1.equals("strawberry5")))
+                ) {
+                    System.out.println(card1 +" : " + card2 + "합이 5임");
+                        // 조건이 참일 때 실행할 코드
+                }
+
+
+
+
 
 
                 p1_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
@@ -73,14 +113,37 @@ public class MainActivity extends AppCompatActivity {
                 int imageResource = getResources().getIdentifier(cardFilp, "drawable", getPackageName());
                 if(imageResource != 0){
                     p2_cardView.setImageResource(imageResource);
+                    card.setCard2(cardFilp);
                 }
+                player2.desCardcount(); //카드 수 1감소
+
+                //카드 카운트가 0이 될 경우를 처리해야 함
+                if(player2.getCardcount() != 0){
+                    //남은카드 텍스트 업데이트
+                    int newCard = player2.getCardcount();
+                    p2_Tcard.setText(Integer.toString(newCard));
+                }else{ //카드 카운트가 0이 될 경우
+                    //
+                    //
+                    //
+                    // 게임 종료에 관한 코드 작성이 필요
+                    //
+                    //
+                    //
+                    //
+                }
+
+
+
+
+
                 p2_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             }
         });
 
 
     }
-    public void card
+
 
 
 }
