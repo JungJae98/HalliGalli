0155e5abc69b3d51f1116aafcc405bfd07258e75
aabds <wogus8880@gmail.com>
2023-06-13 19:33:57 +0900

    11

--- app/src/main/java/kr/co/company/halligalli/MainActivity.java
+++ app/src/main/java/kr/co/company/halligalli/MainActivity.java
@@ -12,10 +12,12 @@ public class MainActivity extends AppCompatActivity {
     int setCardNum = 30;
     Game game = new Game();
     Player player1 = new Player(setCardNum); //기본 카드 숫자를 세팅
-    ImageView p1_cardView;
-    ImageView p2_cardView;
-    TextView p1_Tcard;
-    TextView p2_Tcard;
+    ImageView p1_cardView; // 어떤 카드인지 띄워질 곳
+    ImageView p2_cardView; //어떤 카드인지 띄워질 곳
+    TextView p1_Tcard; //player1 카드 남은 갯수
+    TextView p2_Tcard; //player2 카
+    // 드 남은 갯수
+    String card[2]; //어떤 카드들이 나왔었는지 저장될 배열 0은 player1 1은 player2
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,16 +39,28 @@ public class MainActivity extends AppCompatActivity {
                 int imageResource = getResources().getIdentifier(cardFilp, "drawable", getPackageName());
                 if(imageResource != 0){
                     p1_cardView.setImageResource(imageResource); //0이 아닐때 이미지를 변경한다.
+                    card[0] = cardFilp; //
                 }
                 player1.desCardcount(); //카드수 1 감소
                 //텍스트 새롭게 설정
+
+                //카드 카운트가 0이 될 경우를 처리해야함
                 if(player1.getCardcount() != 0){
                     // 텍스트를 새롭게 설정한다.
                     int newCard = player1.getCardcount();
                     p1_Tcard.setText(Integer.toString(newCard));
-                }else{
-
+                }else{  // 카드 카운트가 0이 될 경우
+                    //
+                    //
+                    //
+                    // 게임 종료에 관한 코드 작성이 필요
+                    //
+                    //
+                    //
+                    //
                 }
+
+
                 p1_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             }
         });
@@ -63,7 +77,10 @@ public class MainActivity extends AppCompatActivity {
                 p2_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
             }
         });
+
+
     }
+    public void card
 
 
 }
