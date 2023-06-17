0c4e920edc9b00e44e0ecfe4f94e436a41bc6f7f
aabds <wogus8880@gmail.com>
2023-06-18 01:44:29 +0900

    1차적인 게임 초기화까지 구현이 됐음

--- app/src/main/java/kr/co/company/halligalli/MainActivity.java
+++ app/src/main/java/kr/co/company/halligalli/MainActivity.java
@@ -1,7 +1,11 @@
 package kr.co.company.halligalli;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -11,9 +15,10 @@ import android.view.animation.Animation;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    int setCardNum = 30;
+    int setCardNum = 10; //플레이어 카드카운트 세팅
     Game game = new Game();
     Player player1 = new Player(setCardNum); //기본 카드 숫자를 세팅
     Player player2 = new Player(setCardNum);
@@ -22,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
     ImageView p2_cardView; //어떤 카드인지 띄워질 곳
     TextView p1_Tcard; //player1 카드 남은 갯수
     TextView p2_Tcard; //player2 카드 남은 갯수
-//    String card[] = {"start", "start"}; //어떤 카드들이 나왔었는지 저장될 배열 0은 player1 1은 player2
+    ImageView p1_bell; //player1 벨
+    ImageView p2_bell; //player2 벨
+    int newCard; //카드 갯수 업데이트를 위한 임시 변수
+    boolean messageSent = false; //핸들러 메시지 수신을 위한 불린형 변수
+
 
 
 
@@ -35,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
         p2_cardView = (ImageView) findViewById(R.id.p2_card);
         p1_Tcard = (TextView) findViewById(R.id.p1_Tcard);
         p2_Tcard = (TextView) findViewById(R.id.p2_Tcard);
-        p1_Tcard.setText(Integer.toString(setCardNum)); //세팅된 카드 숫자를 출력
-
+        p1_bell = (ImageView) findViewById(R.id.p1_bell);
+        p2_bell = (ImageView) findViewById(R.id.p2_bell);
+        p1_Tcard.setText(Integer.toString(setCardNum)); //세팅된 카드 카운팅 숫자를 출력
+        p2_Tcard.setText(Integer.toString(setCardNum)); //세팅된 카드 카운팅 숫자를 출력
 
         MainHandler mHandler = new MainHandler();// 핸들러 사용
         BackgroundThread thread = new BackgroundThread(mHandler); //쓰레드 가져옴
@@ -69,9 +80,22 @@ public class MainActivity extends AppCompatActivity {
                     //카드 카운트가 0이 될 경우를 처리해야함
                     if(player1.getCardcount() != 0){
                         // 남은카드 텍스트 업데이트
-                        int newCard = player1.getCardcount();
+                        newCard = player1.getCardcount();
                         p1_Tcard.setText(Integer.toString(newCard));
                     }else{  // 카드 카운트가 0이 될 경우
+                        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                        builder.setTitle("player2 승리").setMessage("게임을 다시시작하세요");
+                        builder.setPositiveButton("재시작", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Toast.makeText(getApplicationContext(), "재시작한다", Toast.LENGTH_SHORT).show();
+                                Intent intent = getIntent();
+                                finish();
+                                startActivity(intent);
+                            }
+                        });
+                        AlertDialog alertDialog = builder.create();
+                        alertDialog.show();
                         //
                         //
                         //
@@ -83,6 +107,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     p1_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                     game.p1Turn(); // 턴전환
+                    messageSent = false;
                 }
             }
         });
@@ -108,7 +133,7 @@ public class MainActivity extends AppCompatActivity {
                     //카드 카운트가 0이 될 경우를 처리해야 함
                     if(player2.getCardcount() != 0){
                         //남은카드 텍스트 업데이트
-                        int newCard = player2.getCardcount();
+                        newCard = player2.getCardcount();
                         p2_Tcard.setText(Integer.toString(newCard));
 
                     }else{ //카드 카운트가 0이 될 경우
@@ -123,6 +148,7 @@ public class MainActivity extends AppCompatActivity {
                     }
                     p2_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                     game.p2Turn(); //플레이어 턴 전환
+                    messageSent = false;
                 }
             }
         });
@@ -131,6 +157,7 @@ public class MainActivity extends AppCompatActivity {
     // 쓰레드를 이용하여 카드 조건을 확인
     class BackgroundThread extends Thread{
         Handler mhandler;
+
         public BackgroundThread(Handler handler){
             mhandler = handler;
         }
@@ -138,39 +165,46 @@ public class MainActivity extends AppCompatActivity {
             while(true){
                 String card1 = card.getCard1();
                 String card2 = card.getCard2();
-                if ((card1.equals("banana1") && card2.equals("banana4"))
-                        || (card1.equals("banana2") && card2.equals("banana3"))
-                        || (card1.equals("banana3") && card2.equals("banana2"))
-                        || (card1.equals("banana4") && card2.equals("banana1"))
-                        || (card1.equals("plum1") && card2.equals("plum4"))
-                        || (card1.equals("plum2") && card2.equals("plum3"))
-                        || (card1.equals("plum3") && card2.equals("plum2"))
-                        || (card1.equals("plum4") && card2.equals("plum1"))
-                        || (card1.equals("lime1") && card2.equals("lime4"))
-                        || (card1.equals("lime2") && card2.equals("lime3"))
-                        || (card1.equals("lime3") && card2.equals("lime2"))
-                        || (card1.equals("lime4") && card2.equals("lime1"))
-                        || (card1.equals("strawberry1") && card2.equals("strawberry4"))
-                        || (card1.equals("strawberry2") && card2.equals("strawberry3"))
-                        || (card1.equals("strawberry3") && card2.equals("strawberry2"))
-                        || (card1.equals("strawberry4") && card2.equals("strawberry1"))
-                        || (card1.equals("banana5") && !(card2.equals("banana1") || card2.equals("banana2") || card2.equals("banana3") || card2.equals("banana4") || card2.equals("banana5")))
-                        || (card2.equals("banana5") && !(card1.equals("banana1") || card1.equals("banana2") || card1.equals("banana3") || card1.equals("banana4") || card1.equals("banana5")))
-                        || (card1.equals("plum5") && !(card2.equals("plum1") || card2.equals("plum2") || card2.equals("plum3") || card2.equals("plum4") || card2.equals("plum5")))
-                        || (card2.equals("plum5") && !(card1.equals("plum1") || card1.equals("plum2") || card1.equals("plum3") || card1.equals("plum4") || card1.equals("plum5")))
-                        || (card1.equals("lime5") && !(card2.equals("lime1") || card2.equals("lime2") || card2.equals("lime3") || card2.equals("lime4") || card2.equals("lime5")))
-                        || (card2.equals("lime5") && !(card1.equals("lime1") || card1.equals("lime2") || card1.equals("lime3") || card1.equals("lime4") || card1.equals("lime5")))
-                        || (card1.equals("strawberry5") && !(card2.equals("strawberry1") || card2.equals("strawberry2") || card2.equals("strawberry3") || card2.equals("strawberry4") || card2.equals("strawberry5")))
-                        || (card2.equals("strawberry5") && !(card1.equals("strawberry1") || card1.equals("strawberry2") || card1.equals("strawberry3") || card1.equals("strawberry4") || card1.equals("strawberry5")))
-                ) {
-                    System.out.println(card1 +" : " + card2 + "합이 5임");
-                    card.resetFieldCard();
+                if(messageSent == false){
+                    if ((card1.equals("banana1") && card2.equals("banana4"))
+                            || (card1.equals("banana2") && card2.equals("banana3"))
+                            || (card1.equals("banana3") && card2.equals("banana2"))
+                            || (card1.equals("banana4") && card2.equals("banana1"))
+                            || (card1.equals("plum1") && card2.equals("plum4"))
+                            || (card1.equals("plum2") && card2.equals("plum3"))
+                            || (card1.equals("plum3") && card2.equals("plum2"))
+                            || (card1.equals("plum4") && card2.equals("plum1"))
+                            || (card1.equals("lime1") && card2.equals("lime4"))
+                            || (card1.equals("lime2") && card2.equals("lime3"))
+                            || (card1.equals("lime3") && card2.equals("lime2"))
+                            || (card1.equals("lime4") && card2.equals("lime1"))
+                            || (card1.equals("strawberry1") && card2.equals("strawberry4"))
+                            || (card1.equals("strawberry2") && card2.equals("strawberry3"))
+                            || (card1.equals("strawberry3") && card2.equals("strawberry2"))
+                            || (card1.equals("strawberry4") && card2.equals("strawberry1"))
+                            || (card1.equals("banana5") && !(card2.equals("banana1") || card2.equals("banana2") || card2.equals("banana3") || card2.equals("banana4") || card2.equals("banana5")))
+                            || (card2.equals("banana5") && !(card1.equals("banana1") || card1.equals("banana2") || card1.equals("banana3") || card1.equals("banana4") || card1.equals("banana5")))
+                            || (card1.equals("plum5") && !(card2.equals("plum1") || card2.equals("plum2") || card2.equals("plum3") || card2.equals("plum4") || card2.equals("plum5")))
+                            || (card2.equals("plum5") && !(card1.equals("plum1") || card1.equals("plum2") || card1.equals("plum3") || card1.equals("plum4") || card1.equals("plum5")))
+                            || (card1.equals("lime5") && !(card2.equals("lime1") || card2.equals("lime2") || card2.equals("lime3") || card2.equals("lime4") || card2.equals("lime5")))
+                            || (card2.equals("lime5") && !(card1.equals("lime1") || card1.equals("lime2") || card1.equals("lime3") || card1.equals("lime4") || card1.equals("lime5")))
+                            || (card1.equals("strawberry5") && !(card2.equals("strawberry1") || card2.equals("strawberry2") || card2.equals("strawberry3") || card2.equals("strawberry4") || card2.equals("strawberry5")))
+                            || (card2.equals("strawberry5") && !(card1.equals("strawberry1") || card1.equals("strawberry2") || card1.equals("strawberry3") || card1.equals("strawberry4") || card1.equals("strawberry5")))
+                    ) {
+                        System.out.println(card1 +" : " + card2 + "합이 5임");
+                        Message msg = new Message();
+                        msg.what = 1;
 
-                    Message msg = new Message();
-                    msg.what = 1;
+                        mhandler.sendMessage(msg); //핸들러로 메시지를 보냄
+                        System.out.println("check");
 
-                    mhandler.sendMessage(msg);
+                        messageSent = true; //메시지를 보내고나면 다시 못보내기 하기 위해 사용되는 변수
 
+                        //카드를 초기화하기 위해 사용
+                        card.setCard1("a");
+                        card.setCard2("b");
+
+                    }
                 }
                 try{
                     Thread.sleep(100);
@@ -186,13 +220,64 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void handleMessage(Message msg){
             super.handleMessage(msg);
+            int imageResource = getResources().getIdentifier("cardback",  // p1,p2_cardView 바꿀 이미지 세팅
+                    "drawable", getPackageName());
 
+            //조건
             if(msg.what == 1){
-                int imageResource = getResources().getIdentifier("bell", "drawable", getPackageName());
-                if (imageResource != 0){
-                    p1_cardView.setImageResource(imageResource);
-                    p2_cardView.setImageResource(imageResource);
-                }
+                // bell 투명도 조절
+                p1_bell.setAlpha(1f);
+                p2_bell.setAlpha(1f);
+
+                //View 비활성화, bell 활성화
+                p1_cardView.setEnabled(false);
+                p2_cardView.setEnabled(false);
+                p1_bell.setEnabled(true);
+                p2_bell.setEnabled(true);
+                p1_bell.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        newCard = player1.sumCardcount(card.getField_card()); //플레이어 카드에 필드카드 추가
+                        p1_Tcard.setText(Integer.toString(newCard)); // 플레이어 카드 카운트 업데이트
+                        card.resetFieldCard(); // 필드 카드 초기화
+                        
+                        // bell 투명도 조절
+                        p1_bell.setAlpha(0.3f);
+                        p2_bell.setAlpha(0.3f);
+                        msg.what = 0;
+
+                        //View 활성화, bell 비활성화
+                        p1_cardView.setEnabled(true);
+                        p2_cardView.setEnabled(true);
+                        p1_bell.setEnabled(false);
+                        p2_bell.setEnabled(false);
+
+                        p1_cardView.setImageResource(imageResource); // 이미지를 변경
+                        p2_cardView.setImageResource(imageResource); // 이미지를 변경
+                    }
+                });
+                p2_bell.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        newCard = player2.sumCardcount(card.getField_card()); //플레이어 카드에 필드카드 추가
+                        p2_Tcard.setText(Integer.toString(newCard)); // 플레이어 카드 카운트 업데이트
+                        card.resetFieldCard(); // 필드카드 초기화
+                        // bell 투명도 조절
+                        p1_bell.setAlpha(0.3f);
+                        p2_bell.setAlpha(0.3f);
+                        msg.what = 0;
+                        //View 활성화, bell 비활성화
+                        p1_cardView.setEnabled(true);
+                        p2_cardView.setEnabled(true);
+                        p1_bell.setEnabled(false);
+                        p2_bell.setEnabled(false);
+
+                        p1_cardView.setImageResource(imageResource); // 이미지를 변경
+                        p2_cardView.setImageResource(imageResource); // 이미지를 변경
+
+                    }
+                });
+
             }
         }
     }
--- app/src/main/java/kr/co/company/halligalli/Player.java
+++ app/src/main/java/kr/co/company/halligalli/Player.java
@@ -3,19 +3,20 @@ package kr.co.company.halligalli;
 public class Player {
     private int cardcount;
 
-    public Player(int cardcount){
+    public Player(int cardcount){ //플레이어의 카드 카운트 세팅
         this.cardcount = cardcount;
     }
 
-    public void desCardcount(){
+    public void desCardcount(){ //플레이어의 카드 카운트 감소
         this.cardcount--;
     }
 
-    public int getCardcount(){
+    public int getCardcount(){ //플레이어의 카드 카운트 가져오기
         return cardcount;
     }
 
     public int sumCardcount(int sumcard){
-        return cardcount + sumcard;
+        this.cardcount = this.cardcount+ sumcard;
+        return cardcount;
     }
 }
