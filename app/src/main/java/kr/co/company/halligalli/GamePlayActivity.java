17c4865dab8f9dd0e59f58b58ad526f508d77687
aabds <wogus8880@gmail.com>
2023-06-18 19:47:17 +0900

    재시작 안눌리는건 해결 완료

--- app/src/main/java/kr/co/company/halligalli/GamePlayActivity.java
+++ app/src/main/java/kr/co/company/halligalli/GamePlayActivity.java
@@ -1,8 +1,6 @@
 package kr.co.company.halligalli;
-
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -13,10 +11,8 @@ import android.view.animation.AlphaAnimation;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
-
 public class GamePlayActivity extends AppCompatActivity {
-
-    int setCardNum = 20; //플레이어 카드카운트 세팅
+    int setCardNum = 10; //플레이어 카드카운트 세팅
     Game game = new Game();
     Player player1; //기본 카드 숫자를 세팅
     Player player2;
@@ -29,19 +25,16 @@ public class GamePlayActivity extends AppCompatActivity {
     ImageView p2_bell; //player2 벨
     int newCard; //카드 갯수 업데이트를 위한 임시 변수
     boolean messageSent = false; //핸들러 메시지 수신을 위한 불린형 변수
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_play);
-
         p1_cardView = (ImageView) findViewById(R.id.p1_card);
         p2_cardView = (ImageView) findViewById(R.id.p2_card);
         p1_Tcard = (TextView) findViewById(R.id.p1_Tcard);
         p2_Tcard = (TextView) findViewById(R.id.p2_Tcard);
         p1_bell = (ImageView) findViewById(R.id.p1_bell);
         p2_bell = (ImageView) findViewById(R.id.p2_bell);
-
         // 인스턴스로 받아온 카드로 카드 세팅
         setCardNum = getIntent().getIntExtra("playCardNum", 30);
         if(setCardNum == 0){
@@ -56,10 +49,6 @@ public class GamePlayActivity extends AppCompatActivity {
             p2_Tcard.setText(Integer.toString(setCardNum)); //세팅된 카드 카운팅 숫자를 출력
         }
 
-        System.out.println(setCardNum);
-
-
-
 
 
         MainHandler mHandler = new MainHandler();// 핸들러 사용
@@ -68,6 +57,7 @@ public class GamePlayActivity extends AppCompatActivity {
         CardCountThread countThread = new CardCountThread(rhandler); // 카드 카운트를 위한 쓰레드 가져옴
         thread.start();
         countThread.start();
+
         // 카드 뒤집힐 때 애니메이션 효과를 위한 코드
         AlphaAnimation animation = new AlphaAnimation(0.0f, 1.0f);
         animation.setDuration(200);
@@ -87,32 +77,45 @@ public class GamePlayActivity extends AppCompatActivity {
                     }
                     player1.desCardcount(); //카드수 1 감소
                     card.addFieldCard(); // 필드 카드수 증가
-                    p1_cardView.setAlpha(0.6f);
-                    p2_cardView.setAlpha(1f);
-
-
                     //카드 카운트가 0이 될 경우를 처리해야함
-                    if(player1.getCardcount() > 0){
+                    if(player1.getCardcount() != 0){
                         // 남은카드 텍스트 업데이트
                         newCard = player1.getCardcount();
                         p1_Tcard.setText(Integer.toString(newCard));
+                    }else{  // 카드 카운트가 0이 될 경우
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(GamePlayActivity.this);
+//                        builder.setTitle("player2 승리").setMessage("게임을 다시시작하세요");
+//                        builder.setPositiveButton("재시작", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                Toast.makeText(getApplicationContext(), "재시작한다", Toast.LENGTH_SHORT).show();
+//                                Intent intent = getIntent();
+//                                finish();
+//                                startActivity(intent);
+//                            }
+//                        });
+//                        AlertDialog alertDialog = builder.create();
+//                        alertDialog.show();
+                        //
+                        //
+                        //
+                        // 게임 종료에 관한 코드 작성이 필요
+                        //
+                        //
+                        //
+                        //
                     }
-
                     p1_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                     game.p1Turn(); // 턴전환
                     messageSent = false;
-
                 }
             }
         });
-
         p2_cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (game.playerTurn != 0){
-
                     String cardFilp = game.cardFilp();
-
                     // 이미지를 랜덤으로 불러서 가져옴
                     int imageResource = getResources().getIdentifier(cardFilp, "drawable", getPackageName());
                     // 불러온 이미지로 p2_cardView의 이미지를 변경하는 if문 구절
@@ -123,22 +126,38 @@ public class GamePlayActivity extends AppCompatActivity {
                     }
                     player2.desCardcount(); //카드 수 1감소
                     card.addFieldCard(); // 필드 카드수 증가
-                    p1_cardView.setAlpha(1f);
-                    p2_cardView.setAlpha(0.6f);
-
                     //카드 카운트가 0이 될 경우를 처리해야 함
-                    if(player2.getCardcount() > 0){
+                    if(player2.getCardcount() != 0){
                         //남은카드 텍스트 업데이트
                         newCard = player2.getCardcount();
                         p2_Tcard.setText(Integer.toString(newCard));
-                    }else{
 
+                    }else{ //카드 카운트가 0이 될 경우
+//                        AlertDialog.Builder builder = new AlertDialog.Builder(GamePlayActivity.this);
+//                        builder.setTitle("player2 승리").setMessage("게임을 다시시작하세요");
+//                        builder.setPositiveButton("재시작", new DialogInterface.OnClickListener() {
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                Toast.makeText(getApplicationContext(), "재시작한다", Toast.LENGTH_SHORT).show();
+//                                Intent intent = getIntent();
+//                                finish();
+//                                startActivity(intent);
+//                            }
+//                        });
+//                        AlertDialog alertDialog = builder.create();
+//                        alertDialog.show();
+                        //
+                        //
+                        //
+                        // 게임 종료에 관한 코드 작성이 필요
+                        //
+                        //
+                        //
+                        //
                     }
-
                     p2_cardView.setScaleType(ImageView.ScaleType.CENTER_INSIDE);
                     game.p2Turn(); //플레이어 턴 전환
                     messageSent = false;
-
                 }
             }
         });
@@ -156,6 +175,12 @@ public class GamePlayActivity extends AppCompatActivity {
                     Message rmsg = new Message();
                     rmsg.what = 1;
                     rhandler.sendMessage(rmsg); //핸들러로 메시지를 보냄
+                }
+
+                try {
+                    Thread.sleep(200);
+                }catch (Exception e){
+                    e.printStackTrace();
                 }
             }
         }
@@ -179,7 +204,7 @@ public class GamePlayActivity extends AppCompatActivity {
 
                     //이건 핸들러에서 처리를 해야할 듯?
                     AlertDialog.Builder builder = new AlertDialog.Builder(GamePlayActivity.this);
-                    builder.setTitle("승리!").setMessage("게임을 다시시작하세요");
+                    builder.setTitle(winner + "승리!").setMessage("게임을 다시시작하세요");
                     builder.setPositiveButton("재시작", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialogInterface, int i) {
@@ -199,16 +224,9 @@ public class GamePlayActivity extends AppCompatActivity {
         }
     }
 
-    private void restartGame(){
-        Intent intent = getIntent();
-        finish();
-        startActivity(intent);
-    }
-
     // 쓰레드를 이용하여 카드 조건을 확인
     class BackgroundThread extends Thread{
         Handler mhandler;
-
         public BackgroundThread(Handler handler){
             mhandler = handler;
         }
@@ -245,20 +263,16 @@ public class GamePlayActivity extends AppCompatActivity {
                         System.out.println(card1 +" : " + card2 + "합이 5임");
                         Message msg = new Message();
                         msg.what = 1;
-
                         mhandler.sendMessage(msg); //핸들러로 메시지를 보냄
                         System.out.println("check");
-
                         messageSent = true; //메시지를 보내고나면 다시 못보내기 하기 위해 사용되는 변수
-
                         //카드를 초기화하기 위해 사용
                         card.setCard1("a");
                         card.setCard2("b");
-
                     }
                 }
                 try{
-                    Thread.sleep(200);
+                    Thread.sleep(100);
                 } catch (Exception e){
                     e.printStackTrace();
                 }
@@ -284,7 +298,6 @@ public class GamePlayActivity extends AppCompatActivity {
                 // bell 투명도 조절
                 p1_bell.setAlpha(1f);
                 p2_bell.setAlpha(1f);
-
                 //View 비활성화, bell 활성화
                 p1_cardView.setEnabled(false);
                 p2_cardView.setEnabled(false);
@@ -302,13 +315,11 @@ public class GamePlayActivity extends AppCompatActivity {
                         p1_bell.setAlpha(0.3f);
                         p2_bell.setAlpha(0.3f);
                         msg.what = 0;
-
                         //View 활성화, bell 비활성화
                         p1_cardView.setEnabled(true);
                         p2_cardView.setEnabled(true);
                         p1_bell.setEnabled(false);
                         p2_bell.setEnabled(false);
-
                         p1_cardView.setImageResource(imageResource); // 이미지를 변경
                         p2_cardView.setImageResource(imageResource); // 이미지를 변경
                     }
@@ -331,15 +342,11 @@ public class GamePlayActivity extends AppCompatActivity {
                         p2_cardView.setEnabled(true);
                         p1_bell.setEnabled(false);
                         p2_bell.setEnabled(false);
-
                         p1_cardView.setImageResource(imageResource); // 이미지를 변경
                         p2_cardView.setImageResource(imageResource); // 이미지를 변경
-
                     }
                 });
-
             }
         }
     }
-
 }
